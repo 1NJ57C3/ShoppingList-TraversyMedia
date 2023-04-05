389e80ca6692bd7fb31b2748731e575096b40338
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2023-04-05 16:30:01 -0700

    Tutorial Template Complete

--- App.js
+++ App.js
@@ -1,19 +1,54 @@
-import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, StyleSheet, FlatList, Alert} from 'react-native';
+import Header from './components/Header';
+import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
 
 function App() {
+  // TODO Swap out dummy data with proper back-end
+  // TODO Android dark theme doesn't work correctly
+  const [items, setItems] = useState([
+    {id: 0, name: 'Milk'},
+    {id: 1, name: 'Eggs'},
+    {id: 2, name: 'Cheese'},
+    {id: 3, name: 'Prosciutto'},
+  ]);
+
+  let highestId = [...items].sort((a, b) => b.id - a.id)[0].id;
+
+  function handleDeleteItem(itemId) {
+    setItems(prevItems => prevItems.filter(item => item.id !== itemId));
+  }
+
+  function handleAddItem(item) {
+    if (!item) {
+      Alert.alert('Error', 'Please enter an item', {text: 'OK'});
+    } else {
+      setItems(prevItems => [...prevItems, {id: (highestId += 1), name: item}]);
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Goodbye World</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Header title="Shopping List" />
+      <AddItem handleAddItem={handleAddItem} />
+      <FlatList
+        data={items}
+        renderItem={({item}) => (
+          <ListItem
+            key={item.id}
+            item={item}
+            handleDeleteItem={handleDeleteItem}
+          />
+        )}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   text: {
     fontSize: 30,
